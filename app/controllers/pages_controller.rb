edba97818679c03696328da1898e99cfc13c528b
Kaito Kimura <kaitokimura@KaitonoMacBook-Pro.local>
2023-10-09 15:12:25 +0900

    change layout of microposts and top page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    if current_user.nil?
+    else
+      following_users = current_user.following
+      @microposts = Micropost.where(user_id: following_users.pluck(:id))
+    end
   end
 end
