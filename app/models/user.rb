85cc98edb9e91bf813494d5ed5dd0fcc06afa438
Kaito Kimura <kaitokimura@KaitonoMacBook-Pro.local>
2023-10-14 09:10:51 +0900

    add image to profile and micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_one_attached :image
     has_many :microposts, dependent: :destroy
     has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
@@ -26,4 +27,11 @@ class User < ApplicationRecord
         following.include?(other_user)
     end
 
+    def get_image
+        unless image.attached?
+          file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+          image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+        end
+        image
+    end
 end
