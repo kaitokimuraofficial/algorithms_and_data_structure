19413d46bd6f660cabc0c603545cd1b7067ad863
Kaito Kimura <kaitokimura@KaitonoMacBook-Pro.local>
2023-10-16 00:11:22 +0900

    add link into image

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,11 +4,21 @@ class MicropostsController < ApplicationController
         @micropost = Micropost.new(user_id: params[:id])
     end
 
+    def show
+        @micropost = Micropost.find(params[:id])
+        @user = User.find(@micropost.user_id)
+        if current_user == @user or current_user.following?(@user)
+        else
+            redirect_to root_url
+        end
+    end
+
     def index 
         @microposts = Micropost.all
     end
 
     def create 
+        logger.debug(params)
         @micropost = Micropost.new(micropost_params)
         @micropost[:user_id] = current_user.id
         if @micropost.save
@@ -21,6 +31,6 @@ class MicropostsController < ApplicationController
     private    
     # Only allow a list of trusted parameters through.
     def micropost_params
-        params.require(:micropost).permit(:content)
+        params.require(:micropost).permit(:content, :micropost_image)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,18 @@ Rails.application.routes.draw do
   get   'signup',    to: "users#new"
   post  'signup',    to: "users#create"
 
-  get    "login",   to: "sessions#new"
-  post   "login",   to: "sessions#create"
-  get "logout",  to: "sessions#destroy"
+  get    "login",    to: "sessions#new"
+  post   "login",    to: "sessions#create"
+  get    "logout",   to: "sessions#destroy"
   
-  get    "post",     to: "microposts#new"
-  post   "post",     to: "microposts#create"
-
-  get "allpost", to: "microposts#index"
+  get    "allpost",  to: "microposts#index"
 
   
   resources :users do
     member do 
       get 'post', to: 'microposts#new'
       get :following, :followers
+      resources :microposts
     end
   end
   
