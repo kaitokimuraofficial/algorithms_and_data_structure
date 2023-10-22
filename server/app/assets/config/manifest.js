b7a3e23efba72ff562a64490fba2d7fbd3f7e958
Kaito Kimura <kaitokimura@KaitonoMacBook-Pro.local>
2023-10-22 20:41:10 +0900

    add graphql

--- server/app/assets/config/manifest.js
+++ server/app/assets/config/manifest.js
@@ -3,3 +3,5 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../stylesheets
+//= link graphiql/rails/application.css
+//= link graphiql/rails/application.js
--- server/config/application.rb
+++ server/config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +12,8 @@ module FanFan2
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts = nil
+    config.autoloader = :classic
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
