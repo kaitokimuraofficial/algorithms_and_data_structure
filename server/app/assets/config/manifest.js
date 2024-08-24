e8438ccab023f332a37676f79288b0ffaf82af54
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-25 06:12:29 +0900

    Renew

--- server/app/assets/config/manifest.js
+++
@@ -1,7 +0,0 @@
-//= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
-//= link_tree ../stylesheets
-//= link graphiql/rails/application.css
-//= link graphiql/rails/application.js
--- server/app/helpers/users_helper.rb
+++
@@ -1,8 +0,0 @@
-module UsersHelper
-    def user_id_to_name(user_id)
-        user = User.find(user_id)
-        if !user.nil?
-            user.name
-        end
-    end
-end
