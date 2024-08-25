208631cc4ad4e0be4ab8c210b8418c61ee73d0e8
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-26 01:02:06 +0900

    Add tests for doubly linked list

--- clang/tests/doubly-linked-list-test.cc
+++ clang/tests/doubly-linked-list-test.cc
@@ -1,11 +1,11 @@
 extern "C" {
-#include "../src/singly-linked-list.h"
+#include "../src/doubly-linked-list.h"
 }
 
 #include <gtest/gtest.h>
 #include <string>
 
-TEST(SinglyLinkedTest, onlyHead) {
+TEST(DoublyLinkedTest, onlyHead) {
     Node *head = NULL;
 
     EXPECT_EQ(len(head), 0);
@@ -14,7 +14,7 @@ TEST(SinglyLinkedTest, onlyHead) {
     EXPECT_EQ(testing::internal::GetCapturedStdout(), "Head is NULL\n");
 }
 
-TEST(SinglyLinkedTest, insertAndDelete) {
+TEST(DoublyLinkedTest, insertAndDelete) {
     Node *head = NULL;
 
     insertAtEnd(&head, 10);
@@ -23,7 +23,7 @@ TEST(SinglyLinkedTest, insertAndDelete) {
     EXPECT_EQ(len(head), 2);
     testing::internal::CaptureStdout();
     print(head);
-    EXPECT_EQ(testing::internal::GetCapturedStdout(), "10 -> 11\n");
+    EXPECT_EQ(testing::internal::GetCapturedStdout(), "10 <-> 11\n");
 
 
     insertAtBeginning(&head, 2);
@@ -31,7 +31,7 @@ TEST(SinglyLinkedTest, insertAndDelete) {
     EXPECT_EQ(len(head), 3);
     testing::internal::CaptureStdout();
     print(head);
-    EXPECT_EQ(testing::internal::GetCapturedStdout(), "2 -> 10 -> 11\n");
+    EXPECT_EQ(testing::internal::GetCapturedStdout(), "2 <-> 10 <-> 11\n");
 
 
     insertAtIndex(&head, 100, 1);
@@ -39,7 +39,7 @@ TEST(SinglyLinkedTest, insertAndDelete) {
     EXPECT_EQ(len(head), 4);
     testing::internal::CaptureStdout();
     print(head);
-    EXPECT_EQ(testing::internal::GetCapturedStdout(), "2 -> 100 -> 10 -> 11\n");
+    EXPECT_EQ(testing::internal::GetCapturedStdout(), "2 <-> 100 <-> 10 <-> 11\n");
 
 
     deleteAtIndex(&head, 1);
@@ -47,7 +47,7 @@ TEST(SinglyLinkedTest, insertAndDelete) {
     EXPECT_EQ(len(head), 3);
     testing::internal::CaptureStdout();
     print(head);
-    EXPECT_EQ(testing::internal::GetCapturedStdout(), "2 -> 10 -> 11\n");
+    EXPECT_EQ(testing::internal::GetCapturedStdout(), "2 <-> 10 <-> 11\n");
 }
 
 
