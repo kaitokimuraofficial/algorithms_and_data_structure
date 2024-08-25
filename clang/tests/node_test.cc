2f321527801dd21aef0edf211dcf54a3a9b5cf74
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-08-25 16:41:28 +0900

    Add multiple tests

--- clang/tests/node_test.cc
+++ clang/tests/node_test.cc
@@ -4,7 +4,7 @@ extern "C" {
 
 #include <gtest/gtest.h>
 
-TEST(NodeTest, LEN) {
+TEST(NodeTest, LEN1) {
     Node *head = NULL;
 
     insertAtEnd(&head, 10);
@@ -14,6 +14,30 @@ TEST(NodeTest, LEN) {
 
     EXPECT_EQ(len(head), 3);
 }
+
+TEST(NodeTest, LEN2) {
+    Node *head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 11);
+
+    insertAtBeginning(&head, 2);
+
+    EXPECT_EQ(len(head), 3);
+}
+
+
+TEST(NodeTest, LEN3) {
+    Node *head = NULL;
+
+    insertAtEnd(&head, 10);
+    insertAtEnd(&head, 11);
+
+    insertAtBeginning(&head, 2);
+
+    EXPECT_EQ(len(head), 3);
+}
+
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
